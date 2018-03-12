8cd37ca72c421d38a202268b11481b4f93813d6b
Choon Kiat Lee <choonkiat.lee@gmail.com>
2018-03-12 10:32:28 +0000

    Raspberry Pi Web Server

---
+++ Raspberry Pi Web Server/index.py
@@ -0,0 +1,81 @@
+from flask import Flask,render_template,request,Response,jsonify
+import json
+
+import smbus
+import time
+
+app = Flask(__name__)
+
+#time_list = []
+
+@app.route("/graph",methods=["GET"])
+def send_graph_values():
+
+    bus.write_byte(address, ord('a'))
+    time.sleep(0.01)
+    
+    bus.write_byte(address, 129)
+
+    time.sleep(1)
+    bus.write_byte(address, ord('c'))
+    time.sleep(0.01)
+    bus.write_byte(address, 3)
+    
+    time.sleep(1)
+    msg = chr(bus.read_byte(address))
+    print(msg)
+    
+    #stops program
+    bus.write_byte(address, 255)
+
+    response_content = b'data transfer successful!'
+    resp = Response(response_content)    
+    resp.headers['Content-type'] = 'text/plain'
+    resp.headers['Content-Length'] = len(response_content)
+
+    return resp
+
+@app.route("/send",methods=["POST"])
+def send_request():
+    instruction = request.values.get("instruction")
+    command = request.values.get("command")
+
+    if (len(instruction) > 0):
+        instruction = instruction[0]
+    
+    bus.write_byte(address,ord(instruction))
+    time.sleep(0.01)
+
+    bus.write_byte(address,int(command))
+    time.sleep(0.01)
+
+    response_content = 'sent instruction ' + instruction + command
+    resp = Response(response_content)    
+    resp.headers['Content-type'] = 'text/plain'
+    resp.headers['Content-Length'] = len(response_content)
+
+    return resp
+    
+@app.route("/collect",methods = ["GET"])
+def collect_values():
+    msg = chr(bus.read_byte(address))
+
+    response_content = "Collected Value: " + msg
+    resp = Response(response_content)    
+    resp.headers['Content-type'] = 'text/plain'
+    resp.headers['Content-Length'] = len(response_content)
+
+    return resp
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+
+
+if __name__ == '__main__':
+    #app = create_app()
+    bus = smbus.SMBus(1)    
+    address = 0x04
+    
+    app.run(host="192.168.0.102")
