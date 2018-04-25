ef200964b7607a246cd64aed1a8fcb9bbcad76c1
suton5 <suton5@gmail.com>
2018-04-25 16:00:17 +0100

    Trained and saved CNN weights

---
+++ Image/cnn/hazard_detect.py
@@ -0,0 +1,53 @@
+from keras.preprocessing import image
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+import numpy as np
+import sys
+
+
+img_width, img_height = 100, 100
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+img_width, img_height = 100, 100
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.load_weights('hazard.h5')
+
+# predicting images
+path= str(sys.argv[1])
+
+img = image.load_img(path, target_size=(img_width, img_height))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+
+images = np.vstack([x])
+classes = model.predict_classes(images)
+if classes[0][0]==0:
+    print("DANGEROUS")
+else:
+    print("OXIDIZER")
